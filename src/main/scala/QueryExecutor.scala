19cc33f66d3d9dd059b56ef4c8b821871ef3d9a2
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2024-04-18 20:51:17 +0530

    modify code

--- src/main/scala/QueryExecutor.scala
+++ src/main/scala/QueryExecutor.scala
@@ -6,6 +6,14 @@ object QueryExecutor {
     val log = LogManager.getLogger(this.getClass.getName)
     val sparkSession = SessionManager.createSession()
 
+    def getFetchQuery(): String = {
+        """
+          | SELECT mobile
+          | FROM shipping_package_address
+          | WHERE uniware_sp_created >= "2024-01-01"
+          |""".stripMargin
+    }
+
     def getFilterQuery(): String = {
         """
           | SELECT count(distinct(mobile))
@@ -61,9 +69,39 @@ object QueryExecutor {
         println("queryString: " + queryString)
         // Execute query for small set
 
-        // val outputDf = sparkSession.sql(queryString)
-        val outputDf = sparkSession.sql("show tables")
-        outputDf.show(false)
+
+        // val jdbcOptions = getJdbcOptions(queryString)
+        val jdbcOptions = getJdbcOptions("show tables")
+        val rawDataframe = sparkSession.read
+                .format("jdbc")
+                .options(jdbcOptions)
+                .load()
+
+        rawDataframe.show()
+//
+//        println("rawDataframe.count(): " + rawDataframe.count())
+//        rawDataframe.show()
+
+        // Create temp view
+//        rawDataframe.createOrReplaceTempView("raw_dataframe_view")
+//        // Create dataframe from filter query
+//        val filterQuery = getFilterQuery()
+//        val output = sparkSession.sql("select * from raw_dataframe_view")
+//        output.show(false)
+
+        // Full execution
+
+
+//        val fetchQuery = getFetchQuery()
+//        val jdbcOptions = getJdbcOptions(fetchQuery)
+//        val filterQuery = getFilterQuery()
+//
+//        // Read into dataframe
+//        val rawDataframe = sparkSession.read
+//                .format("jdbc")
+//                .options(jdbcOptions)
+//                .load()
+//
 
     }
 }
